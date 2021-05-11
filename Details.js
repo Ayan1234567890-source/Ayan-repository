5514cc3043fc1e34e3c978eb80a79ef72360b042
Ayan1234567890-source <84008587+Ayan1234567890-source@users.noreply.github.com>
2021-05-11 13:46:53 +0530

    Create Details.js

---
+++ Details.js
@@ -0,0 +1,4 @@
+AyansHereDiscordBotdone = true
+if AyansHereDiscordBotdone is true = console.log('"Ayan\'s Here" bot is done!')
+else if AyansHereDiscordBotdone is false console.log(null)
+var secrets = 'Hidden'
